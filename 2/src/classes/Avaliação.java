ce3d736a4f1acca70130d0c0f7e8bc011d0aee14
Leonardo Rodrigues <59699766+leonardomartins92@users.noreply.github.com>
2020-09-30 19:32:36 -0300

    5 & 6

--- 2/src/classes/Avaliação.java
+++ 2/src/classes/Avaliação.java
@@ -3,10 +3,14 @@ package classes;
 public class Avaliação {
     String descrição;
     double valor;
+    String identificador;
+    static int id=1;
 
     public Avaliação(String descrição, double valor) {
         this.descrição = descrição;
         this.valor = valor;
+        identificador="A"+id;
+        id++;
     }
 
     @Override
--- 2/src/classes/Disciplina.java
+++ 2/src/classes/Disciplina.java
@@ -4,10 +4,15 @@ import java.util.ArrayList;
 public class Disciplina {
     String descrição;
     ArrayList<Avaliação> listaAvaliações = new ArrayList<Avaliação>();
+    String identificador;
+    static int id=1;
 
     public Disciplina(String descrição, ArrayList<Avaliação> listaAvaliações) {
         this.descrição = descrição;
         this.listaAvaliações = listaAvaliações;
+        identificador="D"+id;
+        id++;
+
     }
 
     @Override
