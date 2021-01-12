054bb3d77fdc491228a6b55a8624c913181c8ce9
Leonardo Rodrigues <59699766+leonardomartins92@users.noreply.github.com>
2021-01-12 18:30:57 -0300

    Final

--- 7/src/classes/IdentificadorDePessoas.java
+++
@@ -1,8 +0,0 @@
-package classes;
-
-public class IdentificadorDePessoas {
-    public static String identificaPessoas(Object identificado) {
-        String classe =  identificado.getClass().toString();
-        return classe.replace("class classes.","");
-    }
-}
